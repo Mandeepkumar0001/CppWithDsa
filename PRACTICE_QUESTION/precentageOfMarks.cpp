6990a90b68d7c22c679b61f7e41beaea85327860
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-03-09 01:25:32 +0530

    add precentage of 5 subject

---
+++ PRACTICE_QUESTION/precentageOfMarks.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int C=80,
+    OS=80,
+    Java=96,
+    Python=84,
+    JS=90;
+    cout<<"Precentage of marks is:"<<((C+OS+Java+Python+JS)/5)<<endl;
+    return 0;
+}
+
+// Precentage of marks is:86
