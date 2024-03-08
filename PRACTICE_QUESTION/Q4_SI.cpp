70aaa807421eb93a42158a5deb2f439b10e8f9ca
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-03-09 01:44:33 +0530

    Simple Intrest

---
+++ PRACTICE_QUESTION/Q4_SI.cpp
@@ -0,0 +1,10 @@
+// Q4. Calculating Simple Interest
+#include<iostream>
+using namespace std;
+int main(){
+    int p=1000,
+    r=5,t=15;
+    int si=((p*r*t)/100);
+    cout<<si<<endl;
+    return 0;
+}
