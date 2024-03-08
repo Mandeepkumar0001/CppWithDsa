c0fdad5035ae2cd96df7bcfd9ce4423b7a14aa78
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-03-09 02:04:32 +0530

    q.no. 6

---
+++ PRACTICE_QUESTION/Q6.cpp
@@ -0,0 +1,7 @@
+// Q6. How can you output “Alpha” and “Intern” in two different lines in C++?
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Alpha"<<endl<<"Intern";
+    return 0;
+}
