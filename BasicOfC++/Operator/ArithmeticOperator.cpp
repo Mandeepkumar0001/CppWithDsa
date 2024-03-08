7e366c40acd612d4aded4a1614c9b71753ad3738
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-03-09 01:03:38 +0530

    add Arithmetic operator code

---
+++ BasicOfC++/Operator/ArithmeticOperator.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=10;
+    int b=2;
+    cout<<(a+b)<<endl; //10
+    cout<<(a-b)<<endl; //-8
+    cout<<(a*b)<<endl; //20
+    cout<<(a/b)<<endl; //5
+    cout<<(a%b)<<endl; //0
+    return 0;
+}
