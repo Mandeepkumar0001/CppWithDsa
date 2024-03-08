33cb4f2cdaeea0d9996f65a505a8468d3394b9bb
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-03-09 01:33:44 +0530

    add area of circle code

---
+++ PRACTICE_QUESTION/areaOfCircle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float r;
+    cout<<"enter the value of radious";
+    cin>>r;
+    float AOC= 3.14*r*r;
+    cout<<"Area of circle is:"<<AOC<<endl; 
+
+}
+//if r=7.5 then AOC=176.625
