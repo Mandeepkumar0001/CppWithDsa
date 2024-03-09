a11d9ae68d3b9c8994594e7ac75103306b276926
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-03-10 00:15:38 +0530

    add spacialQuestion code

---
+++ 10March24/spacialQuestion.cpp
@@ -0,0 +1,63 @@
+// Given the length and breadth of a rectangle, write a program to find whether numerically the area of the rectangle is greater than its perimeter
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int l=5,b=6;
+//     int areaOfRactangle=(l*b);
+//     int perimeter=(2*(l+b));
+//     cout<<"Area of Ractangle = "<< areaOfRactangle<<endl;
+//     cout<<"Perimeter of Ractangle = "<< perimeter<<endl;
+//     if(areaOfRactangle>perimeter){
+//         cout<<"the area of the rectangle is greater than its perimeter"<<endl;
+//     }
+//     return 0;
+// }
+
+// Write a program to input sides of a triangle and check whether a triangle is equilateral, scalene or isosceles triangle
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int s1,s2,s3;
+//     cout<<"enter the value of s1"<<endl;
+//     cin>>s1;
+//     cout<<"enter the value of s2"<<endl;
+//     cin>>s2;
+//     cout<<"enter the value of s3"<<endl;
+//     cin>>s3;
+//     if(s1==s2 && s1==s3){
+//         cout<<"the triangle is equilateral"<<endl;
+//     }else if(s1==s2 || s2==s3 ||s3==s1){
+//         cout<<"the triangle is isosceles"<<endl;
+//     }else{
+//         cout<<"the triangle is scalene"<<endl;
+//     }
+//     return 0;
+// }
+
+// Given a point (x, y), write a program to find out if it lies on the x-axis, y-axis or at the origin, viz. (0, 0).
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x,y;
+//     cout<<"enter the value of x"<<endl;
+//     cin>>x;
+//     cout<<"enter the value of y"<<endl;
+//     cin>>y;
+//     if(x==0 && y!=0){
+//         cout<<"it lies on the y-axis"<<endl;
+//     }else if(x!=0 && y==0){
+//         cout<<"it lies on the x-axis"<<endl;
+//     }else if(x==0 && y==0){
+//         cout<<"It lies at the origin"<<endl;
+//     }else{
+//         cout<<"It neither lie on x-axis nor on y-axis"<<endl;
+//     }
+//     return 0;
+// }
+
+// Given three points (x1, y1), (x2, y2) and (x3, y3), write a program to check if all the three points fall on one straight line
+
+
