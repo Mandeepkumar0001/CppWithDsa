61ab1a76e615b01d0a06154120b97bab6aba5101
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-03-09 11:34:07 +0530

    update q6

--- PRACTICE_QUESTION/Q6_newline.cpp
+++ PRACTICE_QUESTION/Q6_newline.cpp
@@ -5,3 +5,32 @@ int main(){
     cout<<"Alpha"<<endl<<"Intern";
     return 0;
 }
+
+
+// How much space does the following data types take?
+// int //4 bytes
+// bool //1 bytes
+// float //4 bytes
+
+// Q6. What is the output of this program? 
+// #include<iostream>
+// using namespace std;
+// int main() {
+// int a = 4;
+// int b = 5;
+// a++, b--;
+// cout << ++a << " " << b--; // 6 4
+// }
+
+// Write a program to find the circumference of a circle with radius 10 in C++
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+// float radius = 10;
+// float circumference = 2 * 3.14 * radius;
+// cout << "The circumference of the circle is: " << circumference <<endl;
+// return 0;
+// }
+
+// The circumference of the circle is: 62.8
