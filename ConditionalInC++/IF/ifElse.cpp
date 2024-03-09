40b697c9f1041a8534fc1acc7a5519ef44790332
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-03-09 11:40:28 +0530

    add if else code

---
+++ ConditionalInC++/IF/ifElse.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=4;
+    if(n%2==0){
+        cout<<"This number is even"<<endl;
+    }else{
+        cout<<"This number is odd"<<endl;
+    }
+    return 0;
+}
+
+// This number is even
