cc37c82bd1a15b79085896a27f01d5a1e88d1272
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-03-09 16:30:17 +0530

    add all practice question

---
+++ ConditionalInCpp/practiceQuestion.cpp
@@ -0,0 +1,132 @@
+// Take positive integer input and tell if it is even or odd.
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n=4;
+//     if(n%2==0){
+//         cout<<"This number is even"<<endl; 
+//     }else{
+//         cout<<"This number is odd"<<endl;
+//     }
+//     return 0;
+// }
+
+// Take positive integer input and tell if it is divisible by 5 or not
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n=14;
+//     if(n%5==0){
+//         cout<<"This number is divisible by 5"<<endl;
+//     }else{
+//         cout<<"This number is not divisible by 5"<<endl; 
+//     }
+//     return 0;
+// }
+
+// Given an integer Print the absolute value of that integer
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     cout<<"enter a number"<<endl;
+//     cin>>a;
+//     if(a>0){
+//         cout<<a<<endl;
+//     }else{
+//         cout<<(-a)<<endl;
+//     }
+//     return 0;
+// }
+
+//Given the radius of the circle predict whether numerically area of this circle is larger than the circumference or not
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int r;
+//     cout<<"enter the radious of circle"<<endl;
+//     cin>>r;
+//     int Area=(3.14*r*r);
+//     cout<<"Area of circle is = "<< Area<<endl;
+//     int  circumference=(2*3.14*r);
+//     cout<<"circumference of circle is = "<< circumference<<endl;
+//     if(Area==circumference){
+//         cout<<"The area of circle and circumference are equal"<<endl;
+//     }if(Area>circumference){
+//         cout<<"The area of circle is greater than circumference"<<endl;
+//     }if(Area<circumference){
+//         cout<<"The area of circle is smaller than circumference"<<endl;
+//     }
+//     return 0;
+// }
+
+// 09/03/2024
+// Take positive integer input and tell if it is a three digit number or not.
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"enter a number"<<endl;
+//     cin>>n;
+//     if(n>=100 || n<=999){
+//         cout<<"It is a three digit number"<<endl;
+//     }else{
+//         cout<<"it is not a three digit number"<<endl;
+//     }
+//     return 0;
+// }
+
+// Take positive integer input and tell if it is divisible by 5 and 3.
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"enter a number"<<endl;
+//     cin>>n;
+//     if(n%5==0 && n%3==0){
+//         cout<<"It is a divisible by 5 and 3"<<endl;
+//     }else{
+//         cout<<"it is not divisible by 5 and 3"<<endl;
+//     }
+//     return 0;
+// }
+
+//Take 3 positive integers input and print the greatest of them.
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a=10,b=20,c=30;
+//     if(a>b && a>c){
+//         cout<<"a is largest"<<endl;
+//     }else if(b>c && b>c){
+//         cout<<"b is largest"<<endl;
+//     }else if(c>a && c>a){
+//         cout<<"c is largest"<<endl;
+//     }
+//     return 0;
+// }
+
+// Write a program to check whether a character is an alphabet or not.
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char ch;
+//     cout<<"Enter any character"<<endl;
+//     cin>>ch;
+//     if( (ch>='a' && ch<='z') || (ch>='A' && ch<='Z')){
+//         cout<<"This is an alphabet"<<endl;
+//     }else{
+//         cout<<"This is not an alphabet"<<endl;
+//     }
+//     return 0;
+// }
+
+
