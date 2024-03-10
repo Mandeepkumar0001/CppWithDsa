125a0b6a4ef73745ca048c255a65ea2a8fd35f21
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-03-11 01:50:51 +0530

    basicProblem

--- LoopsInCpp/12march24/basicProblem.cpp
+++ LoopsInCpp/12march24/basicProblem.cpp
@@ -12,7 +12,7 @@
 //     return 0;
 // }
 
-// Print all numbers from 1 to 100 that are divisible by 3
+// Print all numbers from 1 to 100 that are divisible by 3.
 
 // #include<iostream>
 // using namespace std;
