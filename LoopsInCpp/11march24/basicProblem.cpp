90b203b380649f3c25722b753785389f51cfee8a
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-03-11 00:12:24 +0530

    basic problem in loops

---
+++ LoopsInCpp/11march24/basicProblem.cpp
@@ -0,0 +1,68 @@
+// Print all the odd numbers from 1 to 100.
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"the odd numbers from 1 to 100"<<endl;
+//     for(int i=1;i<=100;i++){
+//         if(i%2!=0){
+//             cout<<i<<endl;
+//         }
+//     }
+//     return 0;
+// }
+
+// Print all numbers from 1 to 100 that are divisible by 3
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"all numbers from 1 to 100 that are divisible by 3"<<endl;
+//     for(int i=1;i<=100;i++){
+//         if(i%3==0){
+//             cout<<i<<endl;
+//         }
+//     }
+//     return 0;
+// }
+
+// Print all the even numbers from 1 to 100
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"all the even numbers from 1 to 100"<<endl;
+//     for(int i=1;i<=100;i++){
+//         if(i%2==0){
+//             cout<<i<<endl;
+//         }
+//     }
+//     return 0;
+// }
+
+// Print the table of ‘n’. Here ‘n’ is an integer which the user will input.
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"enter the value of n"<<endl;
+//     cin>>n;
+//     for(int i=1;i<=10;i++){
+//         cout<<n*i<<endl;
+//     }
+//     return 0;
+// }
+
+// Print the table of 19
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int i=1;i<=10;i++){
+//         cout<<19*i<<endl;
+//     }
+//     return 0;
+// }
+
+
