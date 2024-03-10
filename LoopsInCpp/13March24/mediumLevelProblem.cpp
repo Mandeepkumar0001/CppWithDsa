22b8b6753af459f882d68ace626c31594323ab63
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-03-11 01:27:37 +0530

    update

---
+++ LoopsInCpp/13March24/mediumLevelProblem.cpp
@@ -0,0 +1,31 @@
+// Display this AP - 1,3,5,7,9.. upto ‘n’ terms.
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i,n,terms=1;
+//     cout<<"enter the terms"<<endl;
+//     cin>>n;
+//     for(i=1;i<=10;i++){
+//         cout<<terms<<endl;
+//         terms +=2;
+//     }
+//     return 0;
+// }
+
+// Display this GP - 1,2,4,8,16,32,.. upto ‘n’ terms. n 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i,n,terms=1;
+//     cout<<"enter the terms"<<endl;
+//     cin>>n;
+//     for(i=1;i<=n;i++){
+//         cout<<terms<<endl;
+//         terms *=2;
+//     }
+//     return 0;
+// }
+
+// 
---
+++ LoopsInCpp/basicLoops.cpp
@@ -0,0 +1,37 @@
+// FOR LOOP
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         cout<<"Mandeep"<<endl;
+//     }
+//     return 0;
+// }
+
+// While loop
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i=1;
+//     while(i<=5){
+//         cout<<"Mandeep"<<endl;
+//         i++;
+//     }
+//     return 0;
+// }
+
+// DO WHILE LOOP
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i=0;
+//     do{
+//         cout<<i<<endl;
+//         i++;
+//     }
+//     while(i<=10);
+//     return 0;
+// }
