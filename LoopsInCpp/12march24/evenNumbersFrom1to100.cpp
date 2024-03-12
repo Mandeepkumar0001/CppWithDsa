dd340f0e1b0878d69f76d43657047b6bcae6395a
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-03-12 20:19:38 +0530

    even numbers from 1 to 100

---
+++ LoopsInCpp/12march24/evenNumbersFrom1to100.cpp
@@ -0,0 +1,13 @@
+// Print all the even numbers from 1 to 100
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"all the even numbers from 1 to 100"<<endl;
+    for(int i=1;i<=100;i++){
+        if(i%2==0){
+            cout<<i<<endl;
+        }
+    }
+    return 0;
+}
