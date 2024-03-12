01ffde8b7ebc6fc4ef77a79d12138b626f44bee6
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-03-12 20:13:43 +0530

    odd no. 1 to 100

---
+++ LoopsInCpp/12march24/oddNo1to100.cpp
@@ -0,0 +1,13 @@
+Print all the odd numbers from 1 to 100.
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"the odd numbers from 1 to 100"<<endl;
+    for(int i=1;i<=100;i++){
+        if(i%2!=0){
+            cout<<i<<endl;
+        }
+    }
+    return 0;
+}
