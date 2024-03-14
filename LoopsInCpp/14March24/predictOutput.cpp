4ad6865289867eff367b341d18243db1fb3c4f50
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-03-14 19:03:29 +0530

    predict the output

---
+++ LoopsInCpp/14March24/predictOutput.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int main() {
+    while ('a' < 'b')
+        cout << "malayalam is a palindrome" << endl;
+    return 0;
+}
+
+// OUTPUT:- malayalam is a palindrome (Frequently)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+    while (i = 10) {
+        cout << i << endl;
+        i = i + 1;
+    }
+    return 0;
+}
+
+// OUTPUT:- 10 (Frequently)
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+while ('1' < '2')
+cout << "In while loop" << endl;
+}
+
+// OUTPUT:- In while loop (Frequently)
+
+#include <bits/stdc++.h>
+using namespace std;
+int main( ) {
+int t = 10;
+while (t /= 2) {
+cout << "Hello" << endl;
+}
+}
+
+// OUTPUT:- Hello (3 times)
