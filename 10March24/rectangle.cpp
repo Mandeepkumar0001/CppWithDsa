5be366f305ff807052f6878d317f681491afb2c5
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-03-14 17:43:20 +0530

    add ractangle code

--- 10March24/rectangle.cpp
+++ 10March24/rectangle.cpp
@@ -1,5 +1,4 @@
 // Given the length and breadth of a rectangle, write a program to find whether numerically the area of the rectangle is greater than its perimeter
-
 #include<iostream>
 using namespace std;
 int main(){
