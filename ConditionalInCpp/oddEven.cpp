a36305a48272aa064ca79da06f2371cc1adeeb99
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-03-14 17:07:19 +0530

    add odd even code

---
+++ ConditionalInCpp/oddEven.cpp
@@ -0,0 +1,13 @@
+// Take positive integer input and tell if it is even or odd.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n=4;
+    if(n%2==0){
+        cout<<"This number is even"<<endl; 
+    }else{
+        cout<<"This number is odd"<<endl;
+    }
+    return 0;
+}
