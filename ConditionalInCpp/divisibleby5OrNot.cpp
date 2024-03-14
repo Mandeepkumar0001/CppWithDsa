40abcb074c76050de2e31a10fa752691defa2396
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-03-14 17:09:29 +0530

    divisible by 5 or not

---
+++ ConditionalInCpp/divisibleby5OrNot.cpp
@@ -0,0 +1,13 @@
+// Take positive integer input and tell if it is divisible by 5 or not
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n=14;
+    if(n%5==0){
+        cout<<"This number is divisible by 5"<<endl;
+    }else{
+        cout<<"This number is not divisible by 5"<<endl; 
+    }
+    return 0;
+}
