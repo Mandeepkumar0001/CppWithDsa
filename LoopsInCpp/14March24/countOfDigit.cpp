c0ceb20c7e5589d7bcf1f96b4f9857dd1a1e489e
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-03-14 23:22:15 +0530

    count of digit

---
+++ LoopsInCpp/14March24/countOfDigit.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,count=0;
+    cout<<"enter a number"<<endl;
+    cin>>n;
+    while(n>0){
+        n=n/10;
+        count++;
+    }
+    cout<<"total digit: "<<count<<endl;
+    return 0;
+}
