7c73f7959ec9fd59e865612bb3dfaf4b8667b4a6
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-03-14 23:26:34 +0530

    sum of digits

---
+++ LoopsInCpp/14March24/sumOfDigit.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,r,sod=0;
+    cout<<"enter any number"<<endl;
+    cin>>n;
+    while(n>0){
+        r=n%10;
+        sod +=r;
+        n /=10;
+    }
+    cout<<"sum of digit is = "<< soed<<endl;
+    return 0;
+}
