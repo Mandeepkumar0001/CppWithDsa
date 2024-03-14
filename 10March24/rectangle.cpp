77f086f00b85ca88ba7aae60410f7a10a508b042
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-03-14 17:39:32 +0530

    rectangle code

---
+++ 10March24/rectangle.cpp
@@ -0,0 +1,15 @@
+// Given the length and breadth of a rectangle, write a program to find whether numerically the area of the rectangle is greater than its perimeter
+
+#include<iostream>
+using namespace std;
+int main(){
+    int l=5,b=6;
+    int areaOfRactangle=(l*b);
+    int perimeter=(2*(l+b));
+    cout<<"Area of Ractangle = "<< areaOfRactangle<<endl;
+    cout<<"Perimeter of Ractangle = "<< perimeter<<endl;
+    if(areaOfRactangle>perimeter){
+        cout<<"the area of the rectangle is greater than its perimeter"<<endl;
+    }
+    return 0;
+}
