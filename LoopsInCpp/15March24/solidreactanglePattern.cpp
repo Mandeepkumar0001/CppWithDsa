886d941a3139fb5ec639bf7438cc2fe41538099a
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-03-15 23:56:21 +0530

    add solid reactangle pattern code

---
+++ LoopsInCpp/15March24/solidreactanglePattern.cpp
@@ -0,0 +1,13 @@
+// Print the solid reactangle pattern
+
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1;i<=3;i++){
+        for(int j=1;j<=5;j++){
+            cout<<" * ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
