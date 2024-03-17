c1ef88489965067084681b88f9bdbf89418c52d9
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-03-17 13:50:45 +0530

    number square pattern

---
+++ LoopsInCpp/16March2024/numberSquarePattern.cpp
@@ -0,0 +1,21 @@
+/*
+1 2 3 4
+1 2 3 4
+1 2 3 4
+1 2 3 4
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n=1;
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<=4;j++){
+            cout<<n<<" ";
+            n +=1;
+        }
+        n=1;
+        cout<<endl;
+    }
+    return 0;
+}
