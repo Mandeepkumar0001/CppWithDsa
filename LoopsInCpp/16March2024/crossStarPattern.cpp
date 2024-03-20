b576c774a6dfe27752b577a75afb7c033b3223b9
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-03-20 21:48:13 +0530

    star cross pattern

---
+++ LoopsInCpp/16March2024/crossStarPattern.cpp
@@ -0,0 +1,19 @@
+// PRINT THE STAR CROSS PATTERN
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i,j;
+    cout<<"enter a number"<<endl;
+    cin>>n;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            if(i==j || (i+j)==(n+1)){
+                cout<<"*";
+            }else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
