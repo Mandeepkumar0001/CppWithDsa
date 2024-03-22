bac47863bf4e52b8348910b216d957dc06734ecb
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-03-22 23:46:23 +0530

    maximum value in an given array

---
+++ MODULE2/maxInArray.cpp
@@ -0,0 +1,16 @@
+// Find the maximum value out of all the elements in the array.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int m[]={10,5,7,45,12,78};
+    int size=sizeof(m)/sizeof(m[0]);
+    int max=m[0];
+    for(int i=1;i<size;i++){
+        if(m[i]>max){
+            max=m[i];
+        }
+    }
+    cout<<max;
+    return 0;
+}
