91647dc881ad9f86c2b7c547f22784dc37fe873d
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-07-16 23:57:46 +0530

    minimum values in given elements

---
+++ Array/Worksheet2/minimumValue.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the size of array:"<<endl;
+    cin>>n;
+    int arr[n];
+    cout<<"enter the elements of array"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int min=arr[0];
+    for(int i=0;i<n;i++){
+        if(min>arr[i]){
+            min=arr[i];
+        }
+    }
+    cout<<"minimum value is: "<<min;
+}
