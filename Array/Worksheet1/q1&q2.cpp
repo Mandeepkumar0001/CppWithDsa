f1527f523ab8e0707e101c29101e68d50fcf0c7c
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-07-17 19:14:03 +0530

    quesstion 1 and question 2

---
+++ Array/Worksheet1/q1&q2.cpp
@@ -0,0 +1,5 @@
+// q1. Explain what an array is in C++.
+//Ans.=> An Array is a collection of data of the same data type, stored at a contiguous memory location
+
+// q2. Write the syntax to declare an array of 10 integers.
+// ans.=> int marks[10]; 
