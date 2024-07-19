6265a698efef9feec2592439bfbeb3384d446375
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-07-19 23:47:49 +0530

    taking input from user

---
+++ Array/Vector/worksheet1/userInput.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the size of vector ";
+    cin>>n;
+    cout<<"enter the value of vector"<<endl;
+    vector<int> v;
+    for(int i=0;i<n;i++){
+        int p;
+        cin>>p;
+        v.push_back(p);
+    }
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<" ";
+    }
+}
