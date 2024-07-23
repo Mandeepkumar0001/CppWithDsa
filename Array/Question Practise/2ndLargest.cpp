dbfcf77e6ca7ad4f3b6a600372c493125d636e23
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-07-23 17:22:27 +0530

    second largest element

--- Array/Question Practise/2ndLargest.cpp
+++ Array/Question Practise/2ndLargest.cpp
@@ -1,20 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n;
-    cout<<"enter the size of array:"<<endl;
-    cin>>n;
-    int arr[n];
-    cout<<"enter the elements of array"<<endl;
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    int largest=arr[0],slargest=0;
-    for(int i=0;i<n;i++){
+    int arr[]={12,5,15,45,76};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    int largest=arr[0],slargest;
+    for(int i=0;i<size;i++){
         if(largest<arr[i]){
             slargest=largest;
             largest=arr[i];
+        }else if(largest>arr[i] && largest != slargest){
+            slargest=arr[i];
         }
     }
-    cout<<"Second Largest number is: "<<slargest;
+    cout<<" Second largest is: "<<slargest;
+    return 0;
 }
