e039750b97e016a3128d0067a8579c84f80e2303
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-07-23 15:52:17 +0530

    array rotate

--- Array/Vector/Question Practise/rotateArray.cpp
+++ Array/Vector/Question Practise/rotateArray.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main(){
     int k=3;
-    vector<int> arr={5,2,3,7,8,4,9,6};
+    vector<int> arr={5,2,3,7,8,4,9,61,5,8,7,5,2,3};
     vector<int> narr(arr.size());
     for(int i=0;i<arr.size();i++){
         narr[(i+k)%arr.size()]=arr[i];
