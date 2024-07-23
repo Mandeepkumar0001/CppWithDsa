026fb92a02825416e7e807900e499368b839034e
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-07-23 23:39:00 +0530

    second largest element

--- Array/Question Practise/2ndLargest.cpp
+++ Array/Question Practise/2ndLargest.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[]={0,1};
+    int arr[]={0,1,5,3,4,6,21};
     int size=sizeof(arr)/sizeof(arr[0]);
     int largest=arr[0],slargest;
     for(int i=0;i<size;i++){
         if(largest<arr[i]){
             slargest=largest;
             largest=arr[i];
-        }else if(largest>arr[i] && slargest<arr[i]){
+        }else if(slargest<arr[i] && arr[i] !=largest){
             slargest=arr[i];
         }
     }
