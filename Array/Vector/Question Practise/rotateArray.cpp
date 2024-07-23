25015b0c04a2cb9b72d078bb799a84fe51bf80ec
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-07-23 16:00:04 +0530

    rotate array

--- Array/Vector/Question Practise/rotateArray.cpp
+++ Array/Vector/Question Practise/rotateArray.cpp
@@ -1,15 +1,23 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int main(){
-    int k=3;
-    vector<int> arr={5,2,3,7,8,4,9,61,5,8,7,5,2,3};
-    vector<int> narr(arr.size());
-    for(int i=0;i<arr.size();i++){
-        narr[(i+k)%arr.size()]=arr[i];
+void display(vector<int> &v,int k){
+    vector<int> narr(v.size());
+    for(int i=0;i<v.size();i++){
+        narr[(i+k)%v.size()]=v[i];
     }
     cout<<"after rotating: "<<endl;
     for(int i=0;i<narr.size();i++){
         cout<<narr[i]<<" ";
     }
+}
+int main(){
+    vector<int> v1;
+    v1.push_back(1);
+    v1.push_back(2);
+    v1.push_back(3);
+    v1.push_back(4);
+    v1.push_back(5);
+    v1.push_back(6);
+    display(v1,4);
 }
