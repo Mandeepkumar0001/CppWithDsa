89b21a5eacba2534432c46ebb34556e2ced56f0b
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-09-04 01:05:30 +0530

    ensert element

---
+++ ClassWork/insertElement.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> arr={72,80,60,22,50};
+    arr.push_back(40);
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
+}
