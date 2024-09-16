3cf1d570d198ab5b0220d0fa39ce01dbc2c05c54
Mandeepkumar0001 <rajmandeep765@gmail.com>
2024-09-16 22:39:56 +0530

    recursion

---
+++ Recursion1/function.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void gun(){
+    // return;
+    cout<<"mandeep";
+}
+
+void fun(){
+    cout<<"kumar"<<" ";
+    gun();
+}
+int main(){
+    fun();
+}
---
+++ Recursion1/functionCall.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int product(int a, int b){
+    return a*b;
+}
+int main(){
+    // int x=5,y=6;
+    int m=product(5,6);
+    cout<<m;
+}
